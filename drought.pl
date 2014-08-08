dc3a637bde05852c8865e6984c461d3dab6a5d09
Stephen Wetzel <stephen.wetzel1@gmail.com>
2014-08-08 19:40:26 -0400

    download data and make gif

--- drought.pl
+++ drought.pl
@@ -13,45 +13,51 @@ use autodie;
 my $time = time;
 
 my $testing = 0; #set to 1 to prevent actual downloading, 0 normally
-my $stateId = ""; #set this = "" and it will take the id from the command line
+my $stateId = "conus"; #set this = "" and it will take the id from the command line
+my $type = "date"; #20070731_ca_date.png
+my $baseUrl = "http://droughtmonitor.unl.edu/data/pngs/"; #base url of the png files
+my $delay = 3;
+
+
+
 
 if (!$stateId) {$stateId = $ARGV[0];} #grab id from command line if passed
-
+my $temp;
 my $dir = "data.$stateId/";
-mkdir $dir; #a directory for the data files
-
-my $datesUrl = "http://droughtmonitor.unl.edu/data/pngs/"; #this url lists all the dates
-my $datesDump = "dates.txt";
-
+my $datesDump = "dates.txt"; #filename of dates file
 my @fileArray; #stores lines of input files
 my $thisLine; #store individual lines of input file in for loops
+my @dates; #all the valid dates
 
-#lynx -dump -width=9999 -nolist "http://www.imdb.com/title/tt0106145/eprate" >dump.txt
-$temp = "lynx -dump -width=9999 -nolist \"$datesUrl\" > $datesDump";
+if (!-d $dir) {mkdir $dir;} #a directory for the data files
+
+$temp = "lynx -dump -width=9999 -nolist \"$baseUrl\" > $datesDump";
 if (!$testing) {system($temp);} #download page when not testing
-if ($testing) {$datesDump = "dump.txt";} #if testing use saved page
 
 open my $ifile, '<', $datesDump;
 @fileArray = <$ifile>;
 close $ifile;
 foreach $thisLine (@fileArray)
 {#go through the linx dump, gather data
-	#   IMDb > "Star Trek: The Next Generation" (1987)
-	if ($thisLine =~ m/\s+IMDb\s\>\s\"(.+?)\"\s+/)
-	{#this line should contain title
-		$title = $1;
-		print "\nTitle:'$title'";
+	#  2/3/2014  9:51 AM        <dir> 20000118
+	if ($thisLine =~ m/<dir> (\d{8})/)
+	{#this line should contain a date
+		push @dates, $1;
 	}
 }
 
-
-open my $ofile, '>', $allEpisodesFile;
-for (my $i=1; $i <= $numEpisodes; $i++)
-{#go through each episode
-	print $ofile "$temp \t$stats[$i][3]\n";
+foreach my $date (@dates)
+{
+	#http://droughtmonitor.unl.edu/data/pngs/20070731/20070731_ca_date.png
+	my $url = "$baseUrl$date/${date}_${stateId}_$type.png";
+	
+	$temp = "wget -N -P $dir $url";
+	if (!$testing) {system($temp);} #download page when not testing
+	else {print "\n$temp";}
+	
 }
-close $ofile;
 
+`convert -delay $delay -loop 0 $dir*.png $stateId.gif`;
 
 
 print "\nDone\n\n";
